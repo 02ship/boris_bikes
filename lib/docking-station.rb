dd4c32fb0d9f7684464dfd8d051d99bd2b0c41e7
Student <student@Makerss-Air.broadband>
2020-07-09 17:02:28 +0100

    Raises an error message when you try to release a bike from an empty station

--- lib/docking-station.rb
+++ lib/docking-station.rb
@@ -2,7 +2,8 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike
   def release_bike
-    Bike.new
+    raise "No available bikes" unless @bike
+      @bike
   end
   def dock(bike)
     @bike = bike
--- spec/docking-station_spec.rb
+++ spec/docking-station_spec.rb
@@ -2,7 +2,10 @@ require 'docking-station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
   it "releases working bikes" do
-    bike = subject.release_bike
+    bike = Bike.new
+    station = DockingStation.new
+    station.dock(bike)
+    bike = station.release_bike
     expect(bike).to be_working
   end
   it { is_expected.to respond_to(:dock).with(1).argument }
@@ -15,5 +18,10 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+  end
+  describe '#release_bike' do
+    it 'raises an error' do
+      expect {(DockingStation.new).release_bike}.to raise_error "No available bikes"
+    end
   end
 end
